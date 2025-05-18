02b95e55e97ff84db36f8c23d610f663d6b331b0
Yogaraj <yogarajjuju22@gmail.com>
2025-05-18 14:25:16 +0530

    BANKAI

---
+++ src/Pattern5.java
@@ -0,0 +1,16 @@
+public class Pattern5 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    public static  void pattern(int n){
+        for (int row = 0; row <2*n ; row++) {
+            int total = row>n?2*n-row:row;
+            for (int col = 0; col <total ; col++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
