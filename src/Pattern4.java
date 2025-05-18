736e27dfdce1a59793953dbb6688b6fd582498c0
Yogaraj <yogarajjuju22@gmail.com>
2025-05-18 14:09:56 +0530

    BANKAI

--- src/Pattern4.java
+++ src/Pattern4.java
@@ -5,7 +5,7 @@ public class Pattern4 {
     public static void pattern(int n){
         for (int row = 1; row <=n ; row++) {
             for (int col = 1; col < row; col++) {
-                System.out.print(col );
+                System.out.print(col + " " );
 
             }
             System.out.println();
